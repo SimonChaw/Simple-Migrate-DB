e925988fe001fca48467a8e6fb0acdd7d0465d05
SimonChaw <chawblah@gmail.com>
2018-02-07 17:01:10 -0400

    Add admin page

--- includes/admin/admin_page.php
+++ includes/admin/admin_page.php
@@ -10,13 +10,30 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+function render_admin_view(){
+  ?>
+  <div class="wrap">
+    <h1>Simple Database Migration</h1>
+    <hr class="wp-header-end">
+    <div class="" style="width:60%; height:auto; background-color:white;border: 2px solid #ccc;padding:20px;">
+      <a href="#" class="button button-primary">Get Started</a>
+      <!--
+      <form class="" action="index.html" method="post">
+        <h4>Upload URL</h4>
+        <input type="text" name="txtURL" value="">
+      </form>
+      -->
+    </div>
+  </div>
+  <?php
+}
+
 function setup_menu(){
-        add_menu_page( 'Migrate Database', 'Simple Database Migration', 'manage_options', 'migrate-db', 'init', 'dashicons-migrate' );
+  add_menu_page( 'Migrate Database', 'Simple Database Migration', 'manage_options', 'migrate-db', 'init', 'dashicons-migrate', 1 );
 }
 
 function init(){
-        $db_handler = new SMDB_DB_Handler();
-        $db_handler->get_all_tables();
+  render_admin_view();
 }
 
 add_action('admin_menu', 'setup_menu');
